adding the Rossby number switch. Need to fix the convective turnover time calculation

stardate/lhf.py
@@ -47,7 +47,12 @@ def gyro_model(log10_age, bv):
 
 def lnprob(lnparams, *args):
     """
+    The ln-probability function.
     lnparams are [eep, log10(age [yrs]), [Fe/H], ln(distance [kpc]), A_v]
+    If EEP is greater than 425, the star has started evolving up the
+    subgiant branch, so it should have a precise isochronal age and an
+    unreliable gyro age -- shut gyrochronology off!
+    If the Rossby number is greater than 2.16, shut gyrochronology off.
     """
 
     # Transform mass and distance back to linear.
@@ -72,17 +77,46 @@ def lnprob(lnparams, *args):
 
     # Check that the star is cool, that the period is not None, NaN, zero or
     # negative and that it is on the MS.
-    # If EEP is greater than 425, the star has started evolving up the
-    # subgiant branch, so it should have a precise isochronal age and an
-    # unreliable gyro age -- shut gyrochronology off!
+    tau = convective_overturn_time(params[0], params[1], params[2])
     if bv > .45 and period and np.isfinite(period) and 0. < period \
-            and params[0] < 425:
+            and params[0] < 425 and period/tau < 2.16:
         gyro_lnlike = -.5*((period - gyro_model(params[1], bv))
                             /period_err)**2
     else:
         gyro_lnlike = 0
 
     return mod.lnlike(params) + gyro_lnlike + lnpr, lnpr
+
+
+def convective_overturn_time(*args):
+    """
+    Estimate the convective overturn time using equation 11 in Wright et al.
+    (2011): https://arxiv.org/abs/1109.4634
+    log tau = 1.16 - 1.49log(M/M⊙) - 0.54log^2(M/M⊙)
+    (I assume log is log10)
+    params:
+    ------
+    EITHER:
+    mass: (float)
+        Mass in Solar units
+    OR
+    eep: (float)
+        The Equivalent evolutionary point of a star. 355 for the Sun.
+    age: (float)
+        The age of a star in log_10(years).
+    feh: (float)
+        The metallicity of a star.
+    """
+
+    if len(args) > 1:
+        # Convert eep, age and feh to mass (mass will be in Solar mass units)
+        eep, age, feh = args
+        M = mist.mass(eep, age, feh)
+    else:
+        M = args[0]
+
+    log_tau = 1.16 - 1.49*np.log10(M) - .54*(np.log10(M))**2
+    return 10**log_tau
 
 
 def run_mcmc(obs, args, p_init, backend, ndim=5, nwalkers=24, max_n=100000):

tests/test_lhf.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from stardate.lhf import lnprob
+from stardate.lhf import convective_overturn_time
 import stardate
 from isochrones.mist import MIST_Isochrone
 from isochrones import StarModel
@@ -123,10 +124,65 @@ def test_likelihood_rotation():
     # These should be the same because gyro is switched off for giants.
     assert iso_giant_lnprob == gyro_giant_lnprob
 
+
+def test_rossby_switch():
+    """
+    Check that gyrochronology is switch off for stars with Ro > 2.16, ie.
+    with a mass greater than 1.13 at a rotation period of 26 days.
+    Or a rotation period greater than 31 for a mass of 1.
+    """
+
+    iso_params = pd.DataFrame(dict({"teff": (5777, 10),
+                                   "logg": (4.44, .05),
+                                   "feh": (0., .001),
+                                   "parallax": (1., .01)}))  # mas
+    # Set up the StarModel isochrones object.
+    mod = StarModel(mist, **iso_params)
+    lnparams = [355, np.log10(4.56*1e9), 0., np.log(1000), 0.]
+
+    args = [mod, 22., 1., False]  # the lnprob arguments]
+    low_rossby_lnprob = lnprob(lnparams, *args)
+
+    args = [mod, 32., 1., False]  # the lnprob arguments]
+    high_rossby_lnprob = lnprob(lnparams, *args)
+
+    # Make sure that the gyro lnlikelihood is being added for low Rossby
+    # numbers
+    print(low_rossby_lnprob, high_rossby_lnprob)
+    assert low_rossby_lnprob != high_rossby_lnprob
+
+    # Make sure the gyro lnlike is switched off for high rossby numbers
+    args = [mod, 32., 1., True]  # the lnprob arguments]
+    high_rossby_gyro_off_lnprob = lnprob(lnparams, *args)
+
+    print(high_rossby_gyro_off_lnprob, high_rossby_lnprob)
+    assert high_rossby_gyro_off_lnprob == high_rossby_lnprob
+
+
+def test_convective_overturn_timescale():
+    tau = convective_overturn_time(355, np.log10(4.56*1e9), 0.)
+    tau = convective_overturn_time(1)
+    solarRo = 2.16  # van Saders 2016 (2.08 in van Saders 2018)
+    solarProt = 26
+    solartau = solarProt / solarRo
+    # print(solartau)
+    # print(tau)
+    # print("Ro my sun = ", 26/tau, "Solar Ro = 2.16")
+    print(26/convective_overturn_time(1.))  # 1.8
+    print(31/convective_overturn_time(1.))  # 2.14
+    print(32/convective_overturn_time(1.))  # 2.21
+
+    # convective overturn time should go down with mass
+    assert convective_overturn_time(.8) > convective_overturn_time(1.2), \
+        "convective overturn time should go down with mass"
+
+
 if __name__ == "__main__":
-    test_lnprob_higher_likelihood_sun()
-    test_lnprob_higher_likelihood_real()
-    test_likelihood_rotation()
+    # test_lnprob_higher_likelihood_sun()
+    # test_lnprob_higher_likelihood_real()
+    # test_likelihood_rotation()
+    # test_convective_overturn_timescale()
+    test_rossby_switch()
 
     """
     More test ideas