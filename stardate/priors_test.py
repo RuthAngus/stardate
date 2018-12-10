dac0bc1591188541952ec6d1166590f6dc8a2660
RuthAngus <ruth.angus@astro.ox.ac.uk>
2018-12-10 12:28:34 -0500

    adding priors_test.py

---
+++ stardate/priors_test.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import stardate
+from priors import distance_prior
+from stardate.lhf import lnprior
+
+
+def test_lnprior():
+    """
+    Test that the prior on distance increases monotonically between 0 and 3000
+    """
+    print(lnprior([355., 9., 0., 1000., .001]))
+    distances = np.linspace(0, 3000, 1000)
+    probs = []
+    for i in range(len(distances)):
+        probs.append(distance_prior(distances[i]))
+    # probs = distance_prior(distances)
+    # plt.plot(distances, probs)
+    # plt.savefig("test")
+    diffs = np.diff(probs)
+    assert diffs.all() > 0
+
+
+if __name__ == "__main__":
+    test_lnprior()
