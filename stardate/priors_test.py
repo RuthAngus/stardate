b8087e1fd3b6d564c644017d06fdb6a08f55d71a
RuthAngus <ruth.angus@astro.ox.ac.uk>
2018-12-10 12:48:11 -0500

    better prior test

--- stardate/priors_test.py
+++ stardate/priors_test.py
@@ -9,13 +9,17 @@ def test_lnprior():
     """
     Test that the prior on distance increases monotonically between 0 and 3000
     """
-    print(lnprior([355., 9., 0., 1000., .001]))
+    # print(lnprior([355., 9., 0., 1000., .001]))
     distances = np.linspace(0, 3000, 1000)
     probs = []
     for i in range(len(distances)):
         probs.append(distance_prior(distances[i]))
     diffs = np.diff(probs)
+    # plt.plot(distances, probs)
+    # plt.savefig("test")
     assert diffs.all() > 0
+    assert distance_prior(4000) == 0
+    assert distance_prior(-100) == 0
 
 
 if __name__ == "__main__":
