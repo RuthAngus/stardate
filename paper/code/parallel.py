805d28d446463c7708c83ffee8c065542f175afe
Ruth Angus <rangus@rusty1.cm.cluster>
2019-01-30 06:49:49 -0500

    needing to test parallel.py

---
+++ paper/code/parallel.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import h5py
+import tqdm
+import emcee
+
+from isochrones.mist import MIST_Isochrone
+from isochrones import StarModel
+mist = MIST_Isochrone()
+
+import stardate as sd
+from stardate.lhf import lnprob
+
+from multiprocessing import Pool
+
+# Necessary to add cwd to path when script run
+# by SLURM (since it executes a copy)
+sys.path.append(os.getcwd())
+
+def infer_stellar_age(row):
+    df = row[1]
+
+    # Small observational uncertainties are needed (even though the stars
+    # weren't simulated with any) in order to get a good fit.
+    teff_err = 25  # Kelvin
+    logg_err = .05  # dex
+    feh_err = .05  # dex
+    jmag_err = .01 # mags
+    hmag_err = .01  # mags
+    kmag_err = .01  # mags
+    parallax_err = .05  # milliarcseconds
+    prot_err = 1  # Days
+    BV_err = .01  # mags
+
+    #  Infer ages of the simulated stars.
+
+    # Set up the parameter dictionary.
+    iso_params = {"teff": (df.teff, teff_err),
+                "logg": (df.logg, logg_err),
+                "feh": (df.feh, feh_err),
+                "jmag": (df.jmag, jmag_err),
+                "hmag": (df.hmag, hmag_err),
+                "kmag": (df.kmag, kmag_err),
+                "parallax": (df.parallax, parallax_err)}
+
+    # Infer an age with isochrones and gyrochronology.
+
+    # Set up the star object
+    star = sd.Star(iso_params, df.prot, .01, filename="{}_stardate".format(str(int(df.ID)).zfill(4)))
+
+    # Run the MCMC
+    sampler = star.fit(max_n=500)
+
+    # Now infer an age with isochrones only.
+
+    # Set up the star object
+    star_iso = sd.Star(iso_params, df.prot, .01, filename="{}_isochrones".format(str(int(df.ID)).zfill(4)))
+
+    # Run the MCMC
+    sampler = star_iso.fit(max_n=500, iso_only=True)
+
+
+if __name__ == "__main__":
+    #  Load the simulated data file.
+    df = pd.read_csv("data/simulated_data.csv")
+    N = len(df)
+    assert len(df.ID) == len(np.unique(df.ID))
+
+    p = Pool(24)
+    p.map(infer_stellar_age, df.iterrows())
