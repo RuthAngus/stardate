1393935ee6b715eab95ee4d07990af127e3027ab
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2019-04-17 09:30:36 -0400

    running kinda

--- paper/code/parallel.py
+++ paper/code/parallel.py
@@ -8,12 +8,6 @@ import h5py
 import tqdm
 import emcee
 
-# from isochrones.mist import MIST_Isochrone
-# mist = MIST_Isochrone()
-# from isochrones import StarModel, get_ichrone
-# bands = ["B", "V", "J", "H", "K"]
-# mist = get_ichrone("mist", bands=bands)
-
 import stardate as sd
 from stardate.lhf import lnprob
 
@@ -60,8 +54,8 @@ def infer_stellar_age(df):
     # Infer an age with isochrones and gyrochronology.
 
     try:
-        sd_fn = "{}_stardate".format(str(int(df["ID"])).zfill(4))
-        iso_fn = "{}_isochrones".format(str(int(df["ID"])).zfill(4))
+        sd_fn = "simulation_results/{}_stardate".format(
+            str(int(df["ID"])).zfill(4))
 
         if not os.path.exists(sd_fn):
             # Set up the star object
@@ -69,22 +63,10 @@ def infer_stellar_age(df):
                            filename=sd_fn)
 
             # Run the MCMC
-            sampler = star.fit(max_n=300000)
+            sampler = star.fit(max_n=500000)
 
         else:
             print("failed to save file for star. File exists: ", sd_fn)
-
-            # # Now infer an age with isochrones only.
-
-        # if not os.path.exists(iso_fn):
-            # # Set up the star object
-            # star_iso = sd.Star(iso_params, None, None, filename=iso_fn)
-
-            # # Run the MCMC
-            # sampler = star_iso.fit(max_n=200000, iso_only=True)
-
-        # else:
-            # print("failed to save file for star. File exists: ", iso_fn)
 
     except:
         print("failed to save file for star ", str(int(df["ID"])).zfill(4))
@@ -93,11 +75,8 @@ def infer_stellar_age(df):
 if __name__ == "__main__":
     #  Load the simulated data file.
     df = pd.read_csv("data/simulated_data.csv")
-    # df = df.iloc[5:6]
     assert len(df.ID) == len(np.unique(df.ID))
-    ids = np.array([69, 132, 139, 178, 190, 216, 240, 246, 296, 325, 330, 349,
-                    443, 496])
-    df = df.iloc[ids]
+    df = df.iloc[:200]
 
     list_of_dicts = []
     for i in range(len(df)):
@@ -106,7 +85,5 @@ if __name__ == "__main__":
     print(list_of_dicts[0])
     print(len(list_of_dicts))
 
-    p = Pool(14)
-    # list(p.map(infer_stellar_age, list_of_dicts))
+    p = Pool(24)
     list(p.map(infer_stellar_age, list_of_dicts))
-    # p.map(infer_stellar_age, df.iterrows())
